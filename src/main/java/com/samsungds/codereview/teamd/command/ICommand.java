20df197d3585db1f2cd2402c03467a78c50bafc1
taeil <hightail@naver.com>
2022-03-21 13:39:23 +0900

    [Modify] Interface FilePrint -> Print 로 수정

--- src/main/java/com/samsungds/codereview/teamd/command/ICommand.java
+++ src/main/java/com/samsungds/codereview/teamd/command/ICommand.java
@@ -1,11 +1,12 @@
 package com.samsungds.codereview.teamd.command;
 
-import com.samsungds.codereview.teamd.print.FilePrint;
+import com.samsungds.codereview.teamd.print.Print;
 import com.samsungds.codereview.teamd.repo.IRepository;
 
+import java.io.IOException;
+
 public interface ICommand {
-    public Boolean execute(String inputStr);
-    // Interface 변경
-    public void setFilePrint(FilePrint filePrint);
+    public Boolean execute(String inputStr) throws IOException;
+    public void setFilePrint(Print filePrint);
     public void setRepository(IRepository iRepository);
 }
