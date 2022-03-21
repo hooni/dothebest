1559011c086de6b4ccb83ea4b4ee2617eab9fc02
wongeun-lee <leewongeun@gmail.com>
2022-03-21 17:30:45 +0900

    [Modify] 비교 구문 간소화를 위한 option 검색 필드 string으로 변경

--- src/test/java/com/samsungds/codereview/teamd/vo/EmployeeTest.java
+++ src/test/java/com/samsungds/codereview/teamd/vo/EmployeeTest.java
@@ -26,12 +26,12 @@ public class EmployeeTest {
 		assertEquals("KYUMOK", employee.getNameFirst());
 		assertEquals("KIM", employee.getNameLast());
 
-		assertEquals(9777, employee.getPhoneNumMid());
-		assertEquals(6055, employee.getPhoneNumLast());
+		assertEquals("9777", employee.getPhoneNumMid());
+		assertEquals("6055", employee.getPhoneNumLast());
 
-		assertEquals(1998, employee.getBirthdayYear());
-		assertEquals(9, employee.getBirthdayMonth());
-		assertEquals(6, employee.getBirthdayDay());
+		assertEquals("1998", employee.getBirthdayYear());
+		assertEquals("09", employee.getBirthdayMonth());
+		assertEquals("06", employee.getBirthdayDay());
 		
 		assertEquals(0, employee.getEmployeeNumForSort());
 	}
@@ -54,17 +54,17 @@ public class EmployeeTest {
 	void modifyPhoneNumberTest() {
 		employee.setPhoneNum("010-9776-6054");
 		assertEquals("010-9776-6054", employee.getPhoneNum());
-		assertEquals(9776, employee.getPhoneNumMid());
-		assertEquals(6054, employee.getPhoneNumLast());
+		assertEquals("9776", employee.getPhoneNumMid());
+		assertEquals("6054", employee.getPhoneNumLast());
 	}
 
 	@Test
 	void modifyBirthdayTest() {
 		employee.setBirthday("19991007");
 		assertEquals("19991007", employee.getBirthday());
-		assertEquals(1999, employee.getBirthdayYear());
-		assertEquals(10, employee.getBirthdayMonth());
-		assertEquals(7, employee.getBirthdayDay());
+		assertEquals("1999", employee.getBirthdayYear());
+		assertEquals("10", employee.getBirthdayMonth());
+		assertEquals("07", employee.getBirthdayDay());
 	}
 
 	@Test
@@ -89,7 +89,7 @@ public class EmployeeTest {
 		assertEquals(
 				"Employee [employeeNum=18050301, name=KYUMOK KIM, cl=CL2, phoneNum=010-9777-6055, birthday=19980906, certi=PRO, "
 						+"employeeNumForSort=0, nameFirst=KYUMOK, nameLast=KIM, phoneNumMid=9777, phoneNumLast=6055, "
-						+"birthdayYear=1998, birthdayMonth=9, birthdayDay=6]",
+						+"birthdayYear=1998, birthdayMonth=09, birthdayDay=06]",
 				employee.toString());
 	}
 
